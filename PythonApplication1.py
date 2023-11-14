ee05aeb79083421eeed7b8d5894ae85fcfd201ce
sikonghanyang <pwy2373622543@outlook.com>
2023-11-14 23:27:22 +0800

    update

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -5,3 +5,7 @@
 #(x^(i),y^(i)) = i^th training example
 # x --> f  --> y-hat:prediction
 #f_w,b(x) = w x + b  w：权重 b：偏移量
+#(y_hat - y )^2
+#构建成本函数cost function J(w,b)=1/2m Σ(y_hat^(i)-y^(i))^2 ;此处的1/2是为了方便微分
+#goal min（cost function) 
+#
