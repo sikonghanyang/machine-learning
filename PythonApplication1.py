bc2d1c180a887d5957bd6c3cf992e0e8cccc855d
sikonghanyang <pwy2373622543@outlook.com>
2023-11-14 21:25:20 +0800

    gengxin

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,7 @@
-
+#1、用于训练模型的数据集称为训练集  用x表示输入变量，也称为特征或输入特征
+#x="input" Variable also named feature
+#y="output" variable      "target" variable
+# (x,y) = single training example
+#(x^(i),y^(i)) = i^th training example
+# x --> f  --> y-hat:prediction
+#f_w,b(x) = w x + b  w：权重 b：偏移量
